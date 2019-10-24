831d4e00ed66acfbab8f1d9bc763e3de25df6b98
bekir <39104297+inanBekir@users.noreply.github.com>
2019-10-24 19:18:14 +0200

    AS-2 prepare user profile page (#2)
    * Added user profil photo on navbar and sign-up user photo choose
    
    * List-group added profile page
    
    * Added No photo status on product and one size photo on slider
    
    * Added homepage img click and show product
    
    * Fixed homepage less things and show page front end
    
    * Fixed profile page

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,4 @@
 class Product < ApplicationRecord
-    has_attached_file :photo, styles: { medium: "1920x1080>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+    has_attached_file :photo, styles: { medium: "1920x1080>", thumb: "100x100>" }, default_url: "deneme.jpeg"
     validates_attachment_content_type :photo, content_type: /\Aimage\/.*\z/
 end
