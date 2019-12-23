10f46efb11ef8b3c57777ece31d8520a04239bff
bekir <39104297+inanBekir@users.noreply.github.com>
2019-12-23 22:31:00 +0100

    menu bugs fixed (#12)

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,13 @@ Rails.application.routes.draw do
   get 'is_on_sell/index'
   get 'favorites/update'
   get 'favorites/index'
-  #get 'messages/index'
-  get "messages/index"=> "messages/index", :as => "messages_index"
+  get 'messages/index'
   get 'conversations/index'
   devise_for :users, :controllers => { :registrations => 'users/registrations' }
   devise_scope :user do
     get "users/profile"=> "users/registrations#profile", :as => "profile_registration"
   end
-  resources :products do
+  devise_scope :product do
     get "products/isonsell"=> "products/isonsell", :as => "isonsell_product"
     get "products/markassell"=> "products/markassell", :as => "markassell_product"
     get "products/sellingnow"=> "products/sellingnow", :as => "sellingnow_product"
