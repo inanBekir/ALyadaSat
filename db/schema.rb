bd7fd5afd87481588ca4cb8b077ce5f6273018e5
inanBekir <bbekirinan@gmail.com>
2019-12-30 16:41:00 +0100

    schema updated

--- db/schema.rb
+++ db/schema.rb
@@ -91,7 +91,6 @@ ActiveRecord::Schema.define(version: 2019_12_27_121348) do
     t.string "uid"
     t.string "oauth_token"
     t.datetime "oauth_expires_at"
-    t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
   end
